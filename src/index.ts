73140d18daa7d8bfc6a1d9d5f79383cd57617405
Augusto Brito <augustobritodev@icloud.com>
2023-09-10 16:22:35 -0300

    fix: ci cd

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req: any, res: any) => {
   res.json(
     {
-      message: 'Hello World from Typescript v2',
+      message: 'Hello World from Typescript v3',
       date: new Date().toISOString()
     }
   );
